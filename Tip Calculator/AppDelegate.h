cdc35c7b50c360379c3960f12d1fff93a15f9815
Sophia Khezri <skhezri@fb.com>
2018-06-26 13:13:04 -0700

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Sophia Khezri on 6/26/18.
+//  Copyright © 2018 Sophia Khezri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Sophia Khezri on 6/26/18.
+//  Copyright © 2018 Sophia Khezri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
