a4131e8b277763df13db7623f288e28778f35feb
Sophia Khezri <skhezri@fb.com>
2018-06-26 18:56:17 -0700

    Updates

--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -9,6 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *tipControl;
+@property (weak, nonatomic) IBOutlet UILabel *tip;
+@property (weak, nonatomic) IBOutlet UILabel *total;
+
+
 
 @end
 
@@ -17,6 +25,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 
@@ -25,5 +35,65 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing: YES];
+    
+}
+
+- (IBAction)onEdit:(id)sender {
+    
+    NSArray*percentages=@[@(.10), @(.12), @(.15), @(.18), @(.20), @(.22)];
+
+    double tipPercentage= [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    NSLog(@"%ld", (long)self.tipControl.selectedSegmentIndex);
+    NSLog(@"%f", tipPercentage);
+
+    double bill=[self.billField.text doubleValue];
+    double tip= tipPercentage *bill;
+    double total= bill+tip;
+    self.tipLabel.text= [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text=[NSString stringWithFormat:@"$%.2f", total];
+}
+- (IBAction)onEditingBegin:(id)sender {
+    [UIView animateWithDuration:.2 animations:^{
+    self.billField.frame= CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y +30 , self.billField.frame.size.width, self.billField.frame.size.height);
+      
+        
+    }];
+    
+     [UIView animateWithDuration:.2 animations:^{
+         self.tipLabel.alpha=1;
+         self.tipControl.alpha=1;
+         self.totalLabel.alpha=1;
+         self.tip.alpha=1;
+         self.total.alpha=1;
+     
+       }];
+     }
+-
+
+(IBAction)onEditingEnd:(id)sender {
+    CGRect newFrame=self.billField.frame;
+    newFrame.origin.y-=30;
+    [UIView animateWithDuration:.2 animations:^{
+        self.billField.frame= newFrame;
+        
+    }];
+    [UIView animateWithDuration:.2 animations:^{
+        self.tipLabel.alpha=1;
+        self.tipControl.alpha=1;
+        self.totalLabel.alpha=1;
+        self.tip.alpha=1;
+        self.total.alpha=1;
+      
+    }];
+}
+   
+
+
 
 @end
+
+
+
